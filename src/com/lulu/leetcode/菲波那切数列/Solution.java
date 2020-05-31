3806aef12caa43b27843770c44a7dc765366ea8b
zhanglulu <zhanglulu1210@gmail.com>
2020-05-31 15:37:12 +0800

    菲波那切数列 零钱兑换

---
+++ src/com/lulu/leetcode/菲波那切数列/Solution.java
@@ -0,0 +1,72 @@
+package com.lulu.leetcode.菲波那切数列;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * https://leetcode-cn.com/problems/fibonacci-number/
+ */
+public class Solution {
+    public static void main(String[] args) {
+        //System.out.println(new Solution().fib1(100));
+        System.out.println(new Solution().fib2(10));
+        System.out.println(new Solution().fib3(10));
+    }
+
+    /**
+     * 暴力递归
+     * @param N
+     * @return
+     */
+    public int fib1(int N) {
+        if (N == 1 || N == 2) {
+            return 1;
+        }
+        return fib1(N-1) + fib1(N-2);
+    }
+
+    /**
+     * 带备忘录的递归解法
+     * @param N
+     * @return
+     */
+    public int fib2(int N) {
+        return dp2(N, new HashMap<>());
+    }
+
+    private int dp2(int N, Map<Integer, Integer> memo) {
+        if (N < 0) {
+            return 0;
+        }
+        if (N == 1 || N == 2) {
+            return 1;
+        }
+        Integer memoN = memo.get(N);
+        if (memoN != null) {
+            return memoN;
+        }
+        int value = dp2(N - 1, memo) + dp2(N - 2, memo);
+        memo.put(N, value);
+        return value;
+    }
+    /**
+     * dp 数组的迭代解法
+     * @param N
+     * @return
+     */
+    public int fib3(int N) {
+        if (N == 0) {
+            return 0;
+        }
+        if (N == 2 || N == 1) {
+            return 1;
+        }
+        int num = 1, pre = 1, cur = 1;
+        for (int i = 3; i <= N; i++) {
+            num = pre + cur;
+            pre = cur;
+            cur = num;
+        }
+        return num;
+    }
+}
---
+++ src/com/lulu/leetcode/零钱兑换/Solution.java
@@ -0,0 +1,114 @@
+package com.lulu.leetcode.零钱兑换;
+
+import java.util.Arrays;
+import java.util.HashMap;
+
+/**
+ * https://leetcode-cn.com/problems/coin-change/
+ */
+public class Solution {
+    public static void main(String[] args) {
+        //System.out.println("暴力破解 最少个数：" + new Solution().coinChange1(new int[]{1, 2, 5}, 100));
+        //System.out.println("备忘录 最少个数：" + new Solution().coinChange2(new int[]{1,2,5}, 100));
+        System.out.println("DP TABLE 最少个数：" + new Solution().coinChange3(new int[]{1,2,5}, 100));
+        System.out.println("DP TABLE 最少个数：" + new Solution().coinChange3(new int[]{2}, 3));
+    }
+    /**
+     * 暴力破解
+     * @param coins 面值
+     * @param amount 金额
+     * @return 最少个数
+     */
+    public int coinChange1(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return -1;
+        }
+
+        int ret = Integer.MAX_VALUE;
+        for (int i = 0; i < coins.length; i++) {
+            int num = coinChange1(coins, amount - coins[i]);
+            if (num == -1) {
+                continue;
+            }
+            ret = Math.min(num+1, ret);
+        }
+        if (ret == Integer.MAX_VALUE || ret == -1) {
+            return -1;
+        }
+        return ret;
+    }
+
+
+    /**
+     * 备忘录优化
+     * @param coins 面值
+     * @param amount 金额
+     * @return 最少个数
+     */
+    public int coinChange2(int[] coins, int amount) {
+        HashMap<Integer, Integer> memo = new HashMap<>();
+        return dp2(coins, amount, memo);
+    }
+
+    private int dp2(int[] coins, int amount, HashMap<Integer, Integer> memo) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return -1;
+        }
+        Integer memoAmount = memo.get(amount);
+        if (memoAmount != null) {
+            return memoAmount;
+        }
+        int ret = Integer.MAX_VALUE;
+        for (int coin : coins) {
+            int num = dp2(coins, amount - coin, memo);
+            if (num == -1) {
+                continue;
+            }
+            ret = Math.min(num + 1, ret);
+        }
+        if (ret == Integer.MAX_VALUE || ret == -1) {
+            return -1;
+        }
+        memo.put(amount, ret);
+        return ret;
+    }
+
+    /**
+     * dp 数组迭代
+     * @param coins 面值
+     * @param amount 金额
+     * @return 最少个数
+     */
+    public int coinChange3(int[] coins, int amount) {
+        if (amount == 0) {
+            return 0;
+        }
+        if (amount < 0) {
+            return -1;
+        }
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, amount+1);
+        dp[0] = 0;
+        for (int i = 1; i < dp.length; i++) {
+            for (int coin : coins) {
+                int subAmount = i - coin;
+                if (subAmount < 0) {
+                    continue;
+                }
+
+                //选一个最小值
+                dp[i] = Math.min(dp[subAmount]+1, dp[i]);
+            }
+        }
+        if (dp[amount] == amount+1) {
+            return -1;
+        }
+        return dp[amount];
+    }
+}
