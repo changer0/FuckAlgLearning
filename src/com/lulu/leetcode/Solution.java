2763b97e3a7637355db7b905dfab0eb9ce2b140a
zhanglulu <zhanglulu1210@gmail.com>
2020-06-01 08:46:10 +0800

    最长上升子序列

---
+++ src/com/lulu/leetcode/Solution.java
@@ -0,0 +1,10 @@
+package com.lulu.leetcode;
+
+/**
+ * 模板类
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution());
+    }
+}
---
+++ src/com/lulu/leetcode/最长上升子序列/Solution.java
@@ -0,0 +1,70 @@
+package com.lulu.leetcode.最长上升子序列;
+
+import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
+
+import java.util.Arrays;
+
+/**
+ * https://leetcode-cn.com/problems/longest-increasing-subsequence/
+ */
+public class Solution {
+    public static void main(String[] args) {
+        System.out.println(new Solution().lengthOfLIS(new int[]{10,9,2,5,3,7,101,18}));
+    }
+    /**
+     * O(N^2)的解法
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS(int[] nums) {
+        //dp[i] 表示以 nums[i] 这个数结尾的最长递增子序列的长度
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, 1);
+        int maxDp = 0;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] > nums[j]) {
+                    dp[i] =  Math.max(dp[j] + 1, dp[i]);
+                }
+            }
+            maxDp = Math.max(maxDp, dp[i]);
+        }
+        return maxDp;
+    }
+
+    /**
+     * 二分查找解法
+     * @param nums
+     * @return
+     */
+    public int lengthOfLIS2(int[] nums) {
+        int[] top = new int[nums.length];
+        // 牌堆数初始化为 0
+        int piles = 0;
+        for (int i = 0; i < nums.length; i++) {
+            // 要处理的扑克牌
+            int poker = nums[i];
+
+            /***** 搜索左侧边界的二分查找 *****/
+            int left = 0, right = piles;
+            while (left < right) {
+                int mid = (left + right) / 2;
+                if (top[mid] > poker) {
+                    right = mid;
+                } else if (top[mid] < poker) {
+                    left = mid + 1;
+                } else {
+                    right = mid;
+                }
+            }
+            /*********************************/
+
+            // 没找到合适的牌堆，新建一堆
+            if (left == piles) piles++;
+            // 把这张牌放到牌堆顶
+            top[left] = poker;
+        }
+        // 牌堆数就是 LIS 长度
+        return piles;
+    }
+}
