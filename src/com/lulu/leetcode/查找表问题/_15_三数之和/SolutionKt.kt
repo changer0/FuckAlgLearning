134408d065d1804e686c21cc888b2cd1c16afbf6
zhanglulu <zhanglulu1210@gmail.com>
2020-08-04 00:09:53 +0800

    _15_三数之和 好难过啊。。。。真 TM 费劲

--- src/com/lulu/leetcode/查找表问题/_15_三数之和/SolutionKt.kt
+++ src/com/lulu/leetcode/查找表问题/_15_三数之和/SolutionKt.kt
@@ -13,7 +13,8 @@ class SolutionKt {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            SolutionKt()
+            println(SolutionKt().threeSum(intArrayOf(-1, 0, 1, 2, -1, -4)).toString())
+            println(SolutionKt().threeSum(intArrayOf(0, 0, 0, 0, 0)).toString())
         }
     }
 
@@ -24,19 +25,108 @@ class SolutionKt {
      * [-1, 0, 1],
     。* [-1, -1, 2]
      * ]
+     *
+     * 这 TM 暴力解法都不会
      */
-    fun threeSum(nums: IntArray): List<List<Int>> {
+    fun threeSumForce(nums: IntArray): List<List<Int>> {
+        if (nums.size < 3) {
+            return emptyList()
+        }
+        nums.sort()
         val arr = ArrayList<ArrayList<Int>>()
+        var i = 0; var j = 0; var k = 0
+        while (i < nums.size) {
+            if (i == 0 || nums[i] != nums[i-1]) {
+                j = i + 1
+                while (j < nums.size) {
+                    if (j == i + 1 || nums[j] != nums[j-1]) {
+                        k = j + 1
+                        while (k < nums.size) {
+                            if (k == j + 1 || nums[k] != nums[k-1]) {
+                                if (nums[i] + nums[j] + nums[k] == 0) {
+                                    arr.add(arrayListOf(nums[i], nums[j], nums[k]))
+                                }
+                            }
+                            k++
+                        }
+                    }
+                    j++
+                }
+
+            }
+            i++
+        }
         return arr
     }
 
     /**
-     * Swaps x[a] with x[b].
+     * hash 存值，看来是不行！！
      */
-    private fun swap(x: IntArray, a: Int, b: Int) {
-        val t = x[a]
-        x[a] = x[b]
-        x[b] = t
+    fun threeSumHash(nums: IntArray): List<List<Int>> {
+        if (nums.size < 3) {
+            return emptyList()
+        }
+        nums.sort()
+        val arr = ArrayList<ArrayList<Int>>()
+        var i = 0
+        //var j = 0
+        var k = 0
+        var record = hashMapOf<Int, Int>()//保存 num -> index
+        while (i < nums.size ) {
+            if (i == 0 || nums[i] != nums[i-1]) {
+                for (j in i+1 until nums.size){
+                    if (j == i + 1 || nums[j] != nums[j-1]) {
+                        val t = record[0 - nums[i] - nums[j]]
+                        if (t != null) {
+                            arr.add(arrayListOf(nums[i], nums[j], 0 - nums[i] - nums[j]))
+                        }
+                        record[nums[j]] = j
+                    }
+
+                }
+            }
+            record.clear()
+            i++
+        }
+        return arr
+    }
+
+    /**
+     * 对撞指针
+     */
+    fun threeSum(nums: IntArray): List<List<Int>> {
+        if (nums.size < 3) {
+            return emptyList()
+        }
+        nums.sort()
+        val arr = ArrayList<ArrayList<Int>>()
+        var k = 0
+        while (k < nums.size-2) {
+            if (nums[k] > 0) {
+                //如果 > 0，不可能有效
+                break
+            }
+            if (k > 0 && nums[k] == nums[k-1]) {
+                k++
+                continue
+            }
+            var i = k+1; var j = nums.size-1
+            while (i < j) {
+                val sum = nums[k] + nums[i] + nums[j]
+                if (sum == 0) {
+                    arr.add(arrayListOf(nums[k], nums[i], nums[j]))
+                    while (i < j && nums[i] == nums[++i]);
+                    while (i < j && nums[j] == nums[--j]);
+                } else if (sum < 0) {
+                    while (i < j && nums[i] == nums[++i]);
+                } else {
+                    while (i < j && nums[j] == nums[--j]);
+                }
+            }
+            k++
+        }
+
+        return arr
     }
 
 }
