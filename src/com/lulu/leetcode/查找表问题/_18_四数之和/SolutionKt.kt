024c98b264ce2c8a734bb1c806d66be9c3b7543b
zhanglulu <zhanglulu1210@gmail.com>
2020-08-04 16:13:05 +0800

    _18_四数之和 废了老鼻子劲了

--- src/com/lulu/leetcode/查找表问题/_18_四数之和/SolutionKt.kt
+++ src/com/lulu/leetcode/查找表问题/_18_四数之和/SolutionKt.kt
@@ -8,7 +8,8 @@ class SolutionKt {
     companion object {
         @JvmStatic
         fun main(args: Array<String>) {
-            println(SolutionKt().fourSum(intArrayOf(1, 0, -1, 0, -2, 2), 0).toString())
+            //println(SolutionKt().fourSum(intArrayOf(1, 0, -1, 0, -2, 2), 0).toString())
+            println(SolutionKt().fourSum(intArrayOf(-1,0,1,2,-1,-4), -1).toString())
         }
     }
 
@@ -19,23 +20,24 @@ class SolutionKt {
         nums.sort()//先排序
         val arr = ArrayList<ArrayList<Int>>()
         for (k in 0 until nums.size-3) {
-            if (k == 0 || nums[k] != nums[k-1]) {//第一层去重
-                for (p in k+1 until nums.size-2) {
-                    if ( nums[p] == nums[p-1]) {//第二层去重
-                        continue
-                    }
-                    var l = p+1; var r = nums.size-1
-                    while (l < r) {
-                        val complement = nums[k] + nums[p] + nums[l] + nums[r]
-                        if (complement < target) {
-                            while (l < r && nums[l] != nums[++l]);
-                        } else if (complement > target) {
-                            while (l < r && nums[r] != nums[--r]);
-                        } else {
-                            arr.add(arrayListOf(nums[k], nums[p], nums[l], nums[r]))
-                            while (l < r && nums[l] != nums[++l]);
-                            while (l < r && nums[r] != nums[--r]);
-                        }
+            if (k != 0 && nums[k] == nums[k-1]) {//第一层去重
+                continue
+            }
+            for (p in k+1 until nums.size-2) {
+                if (p != k+1 && nums[p] == nums[p-1]) {//第二层去重
+                    continue
+                }
+                var l = p+1; var r = nums.size-1
+                while (l < r) {
+                    val complement = nums[k] + nums[p] + nums[l] + nums[r]
+                    if (complement < target) {
+                        while (l < r && nums[l] == nums[++l]);
+                    } else if (complement > target) {
+                        while (l < r && nums[r] == nums[--r]);
+                    } else {
+                        arr.add(arrayListOf(nums[k], nums[p], nums[l], nums[r]))
+                        while (l < r && nums[l] == nums[++l]);
+                        while (l < r && nums[r] == nums[--r]);
                     }
                 }
             }
